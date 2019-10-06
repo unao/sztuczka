47b8c06e6d8ad0e18f20094bd428c6c4057c7a70
Artur Grzesiak <artur@unao.pl>
2019-10-06 08:59:58 +0200

    normalize audio loudness

---
+++ src/scripts/normalize-audio.ts
@@ -0,0 +1,37 @@
+import * as fs from 'fs'
+import { resolve } from 'path'
+import { from, defer } from 'rxjs'
+import { tap, map, mergeMap } from 'rxjs/operators'
+// @ts-ignore
+import * as normalize from 'ffmpeg-normalize'
+
+const inAssets = (s: string) => resolve(__dirname, '../assets', s)
+
+from(['call', 'sms', 'sound', 'voices'])
+  .pipe(
+    map(inAssets),
+    mergeMap(f => fs.readdirSync(f).map(n => [`${f}/${n}`, `${f}/_${n}`])),
+    tap(([_, o]) => fs.existsSync(o) && fs.unlinkSync(o)),
+    mergeMap(
+      ([input, output]) =>
+        defer(() =>
+          normalize({
+            input: input,
+            output: output,
+            loudness: {
+              normalization: 'ebuR128',
+              target: {
+                input_i: -23,
+                input_lra: 7.0,
+                input_tp: -2.0
+              }
+            }
+          })
+        ).pipe(
+          tap(() => fs.copyFileSync(output, input)),
+          tap(() => fs.unlinkSync(output))
+        ),
+      5
+    )
+  )
+  .subscribe({ error: e => console.error(e) })
