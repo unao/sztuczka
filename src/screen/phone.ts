3328f6cd84468c410143680bfd0b5dc3b15c343e
Artur Grzesiak <artur@unao.pl>
2019-10-09 01:38:20 +0200

    joga & booking images

--- src/screen/phone.ts
+++ src/screen/phone.ts
@@ -10,26 +10,12 @@ import {
   filter,
   finalize
 } from '../common'
-import { initPhoneUI, PhoneUI } from './ui'
+import { initPhoneUI, centerContent } from './ui'
 
 const size = fromEvent(window, 'resize').pipe(
   startWith(true),
   map(() => ({ w: window.innerWidth, h: window.innerHeight }))
 )
-
-const updateLayout = (
-  c: { width: number; height: number },
-  wrap: HTMLElement
-) => (s: { w: number; h: number }) => {
-  const h = s.h - 40
-  const r = h / c.height
-  const w = c.width * r
-
-  wrap.style.top = '20px'
-  wrap.style.left = `${(s.w - w) / 2}px`
-  wrap.style.transform = `scale(${r})`
-  wrap.style.transformOrigin = `top left`
-}
 
 export const phone = (
   root: HTMLDivElement,
@@ -59,5 +45,5 @@ export const phone = (
     callLoud: cs => cs.pipe(tap(phone.loud))
   })
 
-  return merge(size.pipe(tap(updateLayout(phone, phone.wrap))), handle(h))
+  return merge(size.pipe(tap(centerContent(phone, phone.wrap))), handle(h))
 }
